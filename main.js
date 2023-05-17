f883cd9c825190781204f822830b26fbbcf78606
francisco soto Flores <88407964+sotoflore@users.noreply.github.com>
2023-05-17 16:03:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const downloadLink = document.getElementById('download-link');
+  downloadLink.addEventListener('click', () => {
+    const url = "./archivo/HV_Francisco-Soto-Flores.pdf";
+    const filename = 'HV_Francisco-Soto-Flores.pdf';
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const blobUrl = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = blobUrl;
+        a.download = filename;
+        a.click();
+        URL.revokeObjectURL(blobUrl);
+      })
+      .catch(error => console.error(error));
+  });
